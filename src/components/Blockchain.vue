60b6feebf27b87bb5076aac389629963634741f2
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-03-16 17:34:33 -0400

    Adding data to blockchain component

--- src/components/Blockchain.vue
+++ src/components/Blockchain.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    
+  <div class="d-flex flex-column">
+    <p>{{ testdata }}</p>
+    <Block v-for='(block, index) in blockchain' :key='index'>
+    </Block>
   </div>
 </template>
 
 <script>
+import Block from './Block.vue'
 export default {
-  name: "Blockchain"
+  name: "Blockchain",
+  data() {
+    return {
+      testdata: "Hey this is Shailendra Shukla"
+    }
+  },
+  computed: {
+    blockchain() {
+      return this.$store.state.blockchain;
+    }
+  },
+  components: {
+    Block
+  }
 };
 </script>
 
