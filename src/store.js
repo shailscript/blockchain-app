b0645f91b41a38cbe7ba1c388e3ff41759940773
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-03-16 14:28:13 -0400

    Mutations added

--- src/store.js
+++ src/store.js
@@ -18,8 +18,28 @@ export default new Vuex.Store({
     block: '',
 
   },
+
   mutations: {
-    
+    setLoading(state, payload) {
+      state.loading = payload;
+    },
+
+    resetBlockchain(state) {
+      state.blockchain = [
+        {
+          index: 0,
+          timestamp: '',
+          data: 'Shail created this block!',
+          previous_hash: '0x0',
+          nonce: 0
+        }
+      ]
+    },
+
+    pushToBlockchain(state, payload) {
+      state.blockchain.push(payload);
+    }
   },
+  
   actions: {}
 });
