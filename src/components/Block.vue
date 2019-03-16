28298ade990d13bed93ebcba4c5998d3a911bec9
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-03-16 18:42:33 -0400

    Block template design

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,6 +1,16 @@
 <template>
-    <div class="card my-5">
-        {{blockchain[index].data}}
+    <div class="card m-5 p-5">
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <div class="input-group-text">Data</div>
+            </div>
+            <input type="text" class="form-control" placeholder="Block Data" :value="block.data">
+        </div>
+        <p class="text-left"><strong>PREVIOUS HASH</strong> {{ block.previous_hash }}</p>
+        <p class="text-left"><strong>BLOCK HASH</strong> {{ block.hash }}</p>
+        <div class="d-flex justify-content-between">
+            <p class="lead"><strong>{{ blockName }}</strong></p>
+        </div>
     </div>
 </template>
 
@@ -10,8 +20,11 @@ export default {
         'index'
     ],
     computed: {
-        blockchain() {
-            return this.$store.state.blockchain;
+        block() {
+            return this.$store.state.blockchain[this.index];
+        },
+        blockName() {
+            return this.$store.state.blockchain[this.index].index === 0 ? 'GENESIS BLOCK' : `BLOCK #${this.$store.state.blockchain[this.index].index}` 
         }
     }
 }
