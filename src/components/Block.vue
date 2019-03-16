427082cbdd7c9ef97656da89a783d040436896cc
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-03-16 17:36:20 -0400

    Added block component

---
+++ src/components/Block.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        {{blockchain[0].data}}
+    </div>
+</template>
+
+<script>
+export default {
+    
+    computed: {
+        blockchain() {
+            return this.$store.state.blockchain;
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
