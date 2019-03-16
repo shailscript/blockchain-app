ea90417c11d69336ba6e840196a9fb16acf998d0
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-03-16 18:16:35 -0400

    Add block form coded

--- src/components/AddBlock.vue
+++ src/components/AddBlock.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
-        Thus is AddBlock div
+    <div class="card p-5">
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <div class="input-group-text">Data</div>
+            </div>
+            <input type="text" class="form-control" placeholder="Block Data" v-model="data">
+        </div>
+        <button class="btn btn-warning mx-5 mt-5">Create Block</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            data: '',
+        }
+    }
 }
 </script>
 
