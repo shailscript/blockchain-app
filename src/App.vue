937c400319117683d47b6e042c8957b6dc971b61
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-03-19 17:59:17 -0400

    Unnecessary code removed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app" class="container">
     <div class="row">
-      <div class="col-2">
-        Peer 1
-      </div>
-
-      <div class="col-8">
+      <div class="col-10">
         <Blockchain />
       </div>
 
